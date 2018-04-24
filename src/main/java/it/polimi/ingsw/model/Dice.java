ffd1dc8aee2c460bdbd772de64e422e5192b59e8
Ikigai_ <b.armi@live.it>
2018-04-24 16:18:51 +0200

    Created package model, created Dice and Color

---
+++ src/main/java/it/polimi/ingsw/model/Dice.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.model;
+
+public class Dice {
+    private final Color color;
+    private int value;
+
+    public Dice(Color color, int value){
+        this.color = color;
+        this.value = value;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    protected void roll(){
+        //Not implemented yet.
+        return;
+    }
+}
