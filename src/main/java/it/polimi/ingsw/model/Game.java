d4a12dfa729045f6b909f688b89ddd0dc6f1f902
Ikigai_ <b.armi@live.it>
2018-05-07 19:55:44 +0200

    implemented several methods for class Game

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,11 +1,16 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.exceptions.*;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Game {
     private final Factory dealer;
     private final ArrayList<Player> players;
     private Dice[] roundTrack;
+    private int trackIndex;
     private final ToolCard[] toolCards;
     private final PublicGoal[] publicGoals;
     private int nextFirstPlayer;
@@ -15,13 +20,15 @@ public class Game {
         this.dealer = new Factory();
         this.roundTrack = new Dice[10];
         this.toolCards = new ToolCard[3];
+        this.trackIndex = 0;
         this.publicGoals = new PublicGoal[3];
         this.players = new ArrayList<Player>();
         for (int i = 0; i < players.size(); i++)
             this.players.add(players.get(i));
         for (int j = 0; j<3; j++)
             this.publicGoals[j] = dealer.extractPublicGoal();
-        this.currentRound = new Round(dealer.extractPool(2*(players.size()) + 1), players);
+        nextFirstPlayer = (new Random()).nextInt(players.size());
+        nextRound();
     }
 
     public ArrayList<Player> getPlayers() {
@@ -36,37 +43,30 @@ public class Game {
         return publicGoals;
     }
 
-    //Throws Exception
-    public Dice getDiceFromPool(){
-        //TODO
-        return  null;
+    public Dice[] getRoundTrack() {
+        return roundTrack;
     }
 
-    public void setDraftPool(){
-        //TODO
+    public List<Dice> getDiceFromPool(){
+        return currentRound.getDraftPool();
     }
 
-    //Throws Exception
-    public void addDiceToTracker(Dice dice){
-        //TODO
+    private void addDiceToTracker(Dice dice){
+        this.roundTrack[trackIndex++] = new Dice(dice);
     }
 
-    public Player getPlayerByName(String name){
-        //TODO
-        return null;
+    public Player getPlayerByNick(String nick){
+        return (Player) players.stream().filter(player -> player.getNickname().equals(nick));
     }
 
     //Fix UML
     public Round getCurrentRound(){
-        //TODO
-        return null;
+        return new Round(currentRound);
     }
 
-    //Throws Exception
-    public void placeDice(int row, int column, Dice dice){
-        //TODO
+    public void placeDice(int row, int column, Dice dice) throws NoAdjacentDiceException, DiceAlreadyExtractedException, BadAdjacentDiceException, FirstDiceMisplacedException, ConstraintViolatedException, DiceNotInDraftPoolException {
+        currentRound.useDice(row, column, dice);
     }
-
 
     //Throws Exception
     public void tryToolCard(){
@@ -74,7 +74,7 @@ public class Game {
     }
 
     public void suspendPlayer(){
-        //TODO
+        currentRound.suspendPlayer();
     }
 
     public int computeFinalScore(Player player){
@@ -83,7 +83,25 @@ public class Game {
     }
 
     public void nextRound(){
-        //TODO
+        List<Player> roundPlayers = new ArrayList<Player>();
+        List<Dice> draftPool;
+        int size = players.size();
+        int j;
+
+        nextFirstPlayer = (nextFirstPlayer + 1)%size;
+        //Add players
+        for (j = nextFirstPlayer; j == (nextFirstPlayer - 1)%size; j = (j+1)%size)
+            roundPlayers.add(players.get(j));
+        roundPlayers.add(players.get(nextFirstPlayer - 1));
+
+        //Add players in reverse order
+        for (j = (nextFirstPlayer-1)%size; j == nextFirstPlayer; j = (j-1)%size)
+            roundPlayers.add(players.get(j));
+        roundPlayers.add(players.get(nextFirstPlayer));
+
+        draftPool = currentRound.getDraftPool();
+        addDiceToTracker(draftPool.get((new Random()).nextInt(draftPool.size())));
+        currentRound = new Round(dealer.extractPool(2*(size) + 1), roundPlayers);
     }
 
 }
