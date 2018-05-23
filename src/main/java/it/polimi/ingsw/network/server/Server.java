97c001b4e388c46dd30c2672d92335eaac0df28a
marco96casa <marcocasarotto96@gmail.com>
2018-05-24 00:05:11 +0200

    Minor fix

--- src/main/java/it/polimi/ingsw/network/server/Server.java
+++ src/main/java/it/polimi/ingsw/network/server/Server.java
@@ -34,7 +34,7 @@ public class Server {
         while(true) {
             try {
                 clientSocket = serverSocket.accept();
-                Logger.print("Connection over server: " + clientSocket.getRemoteSocketAddress().toString());
+                Logger.print("Connection over socket: " + clientSocket.getRemoteSocketAddress().toString());
                 executor.submit(new SocketHandler(clientSocket, usersHandler));
             } catch (IOException e) {
                 e.printStackTrace();
