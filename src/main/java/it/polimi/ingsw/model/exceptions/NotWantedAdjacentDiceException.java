2817ac3686f477fd68c818eef27fc492665a8a72
mario-arduini <mario.arduini.gen@gmail.com>
2018-06-04 16:16:32 +0200

    added exception

---
+++ src/main/java/it/polimi/ingsw/model/exceptions/NotWantedAdjacentDiceException.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.model.exceptions;
+
+public class NotWantedAdjacentDiceException extends Exception {
+    public NotWantedAdjacentDiceException(){
+        super();
+    }
+
+    public NotWantedAdjacentDiceException(String s){
+        super(s);
+    }
+}
