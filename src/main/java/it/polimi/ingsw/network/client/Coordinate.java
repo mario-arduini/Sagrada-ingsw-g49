75f0269ab67c9d08b843d5915b3e1df990df48c3
Ikigai_ <b.armi@live.it>
2018-06-10 14:48:34 +0200

    Pinza Sgrossatrice fix, Client was sending wrong coordinates

--- src/main/java/it/polimi/ingsw/network/client/Coordinate.java
+++ src/main/java/it/polimi/ingsw/network/client/Coordinate.java
@@ -4,9 +4,10 @@ class Coordinate {
     private final int row;
     private final int column;
 
+    //TODO: have to throw an exception according to schema size
     Coordinate(int row, int column){
-        this.row = row;
-        this.column = column;
+        this.row = row-1;
+        this.column = column-1;
     }
 
     int getRow(){
