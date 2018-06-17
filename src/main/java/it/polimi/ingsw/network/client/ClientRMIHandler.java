7619dc97eef28e5813e7f44370b4f2c30ca9b18f
Ikigai_ <b.armi@live.it>
2018-06-17 23:54:39 +0200

    RMI introduction, implementation of a simple function

--- src/main/java/it/polimi/ingsw/network/client/ClientRMIHandler.java
+++ src/main/java/it/polimi/ingsw/network/client/ClientRMIHandler.java
@@ -1,17 +1,13 @@
 package it.polimi.ingsw.network.client;
 
-import it.polimi.ingsw.network.server.rmi.LoginInterface;
-
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class ClientRMIHandler extends UnicastRemoteObject implements ClientRMIInterface {
 
 
-    public ClientRMIHandler() throws RemoteException, NotBoundException {
+    public ClientRMIHandler() throws RemoteException {
         
     }
 
--- src/main/java/it/polimi/ingsw/network/server/rmi/LoginInterface.java
+++ src/main/java/it/polimi/ingsw/network/server/rmi/LoginInterface.java
@@ -1,8 +1,13 @@
 package it.polimi.ingsw.network.server.rmi;
 
+import it.polimi.ingsw.network.client.ClientRMIInterface;
 import it.polimi.ingsw.network.server.ConnectionHandler;
 import it.polimi.ingsw.network.server.exception.LoginFailedException;
 
-public interface LoginInterface extends java.rmi.Remote{
-    void login(String name, String password, ConnectionHandler connectionHandler) throws java.rmi.RemoteException, LoginFailedException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface LoginInterface extends Remote {
+    //void login(String name, String password, ConnectionHandler connectionHandler) throws RemoteException, LoginFailedException;
+    void hello(String message) throws RemoteException;
 }
