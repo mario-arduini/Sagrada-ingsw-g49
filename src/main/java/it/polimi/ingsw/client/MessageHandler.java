57c83b21154f223e936c3ba79c825fe0410f341c
Ikigai_ <b.armi@live.it>
2018-07-08 21:59:47 +0200

    modified config file for server

--- src/main/java/it/polimi/ingsw/client/MessageHandler.java
+++ src/main/java/it/polimi/ingsw/client/MessageHandler.java
@@ -10,11 +10,18 @@ import java.io.BufferedReader;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * A MessageHandler object retrieves messages from a json file.
+ */
 public class MessageHandler {
     private static HashMap<String,String> messages;
 
     private MessageHandler(){}
 
+    /**
+     * Initiates the MessageHandler in order to get prompts from a language.
+     * @param language the name of the file to read.
+     */
     private static void init(String language){
         if(messages == null){
             List<BufferedReader> languages = FilesUtil.listFiles(language, 1);
@@ -31,6 +38,11 @@ public class MessageHandler {
         }
     }
 
+    /**
+     * Gets a message from a json config file.
+     * @param from the key of the json object
+     * @return the value associated to the key.
+     */
     public static String get(String from){
         init("english");
         return messages.get(from);
